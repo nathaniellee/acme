51b9024d50d0557cfb2498559899c85f14dbe667
Nathaniel Lee <nlee@appnexus.com>
2014-08-20 19:34:29 -0400

    Streamlined or removed comments. Reorganized the unit tests just a little bit.

--- Set/Set.js
+++ Set/Set.js
@@ -12,30 +12,16 @@
 			}
 		}
 
-		/***********************************************************************
-		 *  
-		 *  _isActualNaN :: * -> boolean
-		 *  
-		 *  Determine whether a value is an actual NaN value as opposed to a
-		 *  value that just happens to not be a number. We need to test for this
-		 *  because the array `indexOf` method always returns -1 for actual NaN
-		 *  values (thus we need special accommodations for that value).
-		 *  
+		/*
+		 * Determine whether a value is an actual NaN value as opposed to a
+		 * value that just happens to not be a number. We need to test for this
+		 * because the array `indexOf` method always returns -1 for actual NaN
+		 * values (thus we need special accommodations for that value).
 		 */
 		function _isActualNaN(value) {
 			return typeof value === 'number' && value !== value;
 		}
 
-		/***********************************************************************
-		 *  
-		 *  _addValue :: * -> ()
-		 *  
-		 *  Add the specified value to the set if it isn't already a member. We
-		 *  need to test if it's an actual NaN value since we store the index of
-		 *  that type of value separately to get around the issue with array
-		 *  `indexOf` and NaN values.
-		 *  
-		 */
 		function _addValue(value) {
 			if (_isActualNaN(value)) {
 				if (_indexForNaNValue === -1) {
@@ -49,26 +35,12 @@
 			_setSize();
 		}
 
-		/***********************************************************************
-		 *  
-		 *  _clear :: () -> ()
-		 *  
-		 *  Clear out all values that have been added to this instance.
-		 *  
-		 */
 		function _clear() {
 			_indexForNaNValue = -1;
 			_values = [];
 			_setSize();
 		}
 
-		/***********************************************************************
-		 *  
-		 *  _deleteValue :: * -> boolean
-		 *  
-		 *  Remove the specified value if it exists in this instance.
-		 *  
-		 */
 		function _deleteValue(value) {
 			var index = _isActualNaN(value) ? _indexForNaNValue : _values.indexOf(value);
 
@@ -80,25 +52,10 @@
 			return false;
 		}
 
-		/***********************************************************************
-		 *  
-		 *  _hasValue :: * -> boolean
-		 *  
-		 *  Determine whether the value has been added to this instance.
-		 *  
-		 */
 		function _hasValue(value) {
 			return (_isActualNaN(value) ? _indexForNaNValue : _values.indexOf(value)) !== -1;
 		}
 
-		/***********************************************************************
-		 *  
-		 *  _setSize :: () -> ()
-		 *  
-		 *  Set the public `size` property of the Set instance to the length of
-		 *  the internal `_values` array.
-		 *  
-		 */
 		function _setSize() {
 			_self.size = _values.length;
 		}
--- Set/Set.spec.js
+++ Set/Set.spec.js
@@ -5,112 +5,118 @@ describe('ES6 Set shim', function () {
 		_module = new Set(params);
 	}
 
-	describe('Instantiation', function () {
-		function getInstantiator(params) {
-			return function () {
-				_instantiate(params);
-			};
-		}
+	describe('Initialization', function () {
+		describe('new Set(*)', function () {
+			function getInstantiator(params) {
+				return function () {
+					_instantiate(params);
+				};
+			}
 
-		it('can be instantiated without any parameters.', function () {
-			expect(getInstantiator()).to.not.throw();
+			it('can be instantiated without any parameters.', function () {
+				expect(getInstantiator()).to.not.throw();
+			});
+
+			it('can be instantiated with parameters of any type of value.', function () {
+				['abc', 123, true, false, function () {}, [], {}, NaN, null, undefined].forEach(function (value) {
+					expect(getInstantiator(value)).to.not.throw();
+				});
+			});
+
+			it('accepts an array of values.', function () {
+				_instantiate([
+					123,
+					'123',
+					NaN
+				]);
+				expect(_module.has(123)).to.be.true;
+				expect(_module.has('123')).to.be.true;
+				expect(_module.has(NaN)).to.be.true;
+				expect(_module.size).to.equal(3);
+			});
 		});
+	});
 
-		it('can be instantiated with paramters of any type of value.', function () {
-			['abc', 123, true, false, function () {}, [], {}, NaN, null, undefined].forEach(function (value) {
-				expect(getInstantiator(value)).to.not.throw();
+	describe('public methods', function () {
+		describe('clear()', function () {
+			it('deletes every value from the instance.', function () {
+				_instantiate();
+				_module.add(123);
+				_module.clear();
+				expect(_module.size).to.equal(0);
 			});
 		});
 
-		it('accepts an array of arrays of values.', function () {
-			_instantiate([
-				123,
-				'123',
-				NaN
-			]);
-			expect(_module.has(123)).to.be.true;
-			expect(_module.has('123')).to.be.true;
-			expect(_module.has(NaN)).to.be.true;
-			expect(_module.size).to.equal(3);
+		describe('delete(value)', function () {
+			it('deletes the specified value, if set.', function () {
+				_instantiate();
+				_module.add(123);
+				_module.delete(123);
+				expect(_module.size).to.equal(0);
+			});
+
+			it('returns false.', function () {
+				_instantiate();
+				_module.add(123);
+				expect(_module.delete(123)).to.be.false;
+			});
+		});
+
+		describe('has(value)', function () {
+			it('returns true if the value has been added.', function () {
+				_instantiate();
+				_module.add(123);
+				expect(_module.has(123)).to.be.true;
+			});
+
+			it('returns false if the value has not been added.', function () {
+				_instantiate();
+				expect(_module.has(123)).to.be.false;
+			});
+		});
+
+		describe('add(value)', function () {
+			it('adds the specified value to the instance if it has not yet been added the instance.', function () {
+				_instantiate();
+				_module.add(123);
+				expect(_module.has(123)).to.be.true;
+				expect(_module.size).to.equal(1);
+				_module.add(123);
+				expect(_module.size).to.equal(1);
+			});
+
+			it('differentiates between string and number representations of the same numerical value.', function () {
+				_instantiate();
+				_module.add(123);
+				_module.add('123');
+				expect(_module.size).to.equal(2);
+			});
+
+			it('treats different NaN values as the same value.', function () {
+				var a = NaN;
+				var b = NaN;
+
+				_instantiate();
+				_module.add(a);
+				_module.add(b);
+				expect(_module.has(a)).to.be.true;
+				expect(_module.has(b)).to.be.true;
+				expect(_module.size).to.equal(1);
+			});
 		});
 	});
-
-	describe('.clear :: () -> ()', function () {
-		it('deletes every value from the instance.', function () {
-			_instantiate();
-			_module.add(123);
-			_module.clear();
-			expect(_module.size).to.equal(0);
-		});
-	});
-
-	describe('.delete :: * -> ()', function () {
-		it('deletes the specified value, if set.', function () {
-			_instantiate();
-			_module.add(123);
-			_module.delete(123);
-			expect(_module.size).to.equal(0);
-		});
-
-		it('returns false.', function () {
-			_instantiate();
-			_module.add(123);
-			expect(_module.delete(123)).to.be.false;
-		});
-	});
-
-	describe('.has :: * -> boolean', function () {
-		it('returns true if the value has been added.', function () {
-			_instantiate();
-			_module.add(123);
-			expect(_module.has(123)).to.be.true;
-		});
-
-		it('returns false if the value has not been added.', function () {
-			_instantiate();
-			expect(_module.has(123)).to.be.false;
-		});
-	});
-
-	describe('.add :: * -> ()', function () {
-		it('adds the specified value to the instance if it has not yet been added the instance.', function () {
-			_instantiate();
-			_module.add(123);
-			expect(_module.has(123)).to.be.true;
-			expect(_module.size).to.equal(1);
-			_module.add(123);
-			expect(_module.size).to.equal(1);
-		});
-
-		it('differentiates between string and number representations of the same numerical value.', function () {
-			_instantiate();
-			_module.add(123);
-			_module.add('123');
-			expect(_module.size).to.equal(2);
-		});
-
-		it('treats different NaN values as the same value.', function () {
-			var a = NaN;
-			var b = NaN;
-
-			_instantiate();
-			_module.add(a);
-			_module.add(b);
-			expect(_module.has(a)).to.be.true;
-			expect(_module.has(b)).to.be.true;
-			expect(_module.size).to.equal(1);
-		});
-	});
-
-	describe('.size :: number', function () {
-		it('represents the number of values that have been added.', function () {
-			_instantiate();
-			_module.add(123);
-			expect(_module.size).to.equal(1);
-			_module.add('123');
-			expect(_module.size).to.equal(2);
-			_module.add(NaN);
-			expect(_module.size).to.equal(3);
+	
+	describe('public properties', function () {
+		describe('size', function () {
+			it('represents the number of values that have been added.', function () {
+				_instantiate();
+				_module.add(123);
+				expect(_module.size).to.equal(1);
+				_module.add('123');
+				expect(_module.size).to.equal(2);
+				_module.add(NaN);
+				expect(_module.size).to.equal(3);
+			});
 		});
 	});
 });
